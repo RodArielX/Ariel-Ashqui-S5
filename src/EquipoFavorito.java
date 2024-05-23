f26e1a9870fbced88bc0d9f189ace2d9b773dd8c
POO <ariel.ashqui@epn.edu.ec>
2024-05-23 08:52:53 -0500

    Semana5

---
+++ src/EquipoFavorito.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class EquipoFavorito {
+    String nombre;
+    String color;
+    int años;
+
+    public EquipoFavorito(String nombre, String color, int años){
+        this.nombre = nombre;
+        this.color = color;
+        this.años = años;
+    }
+    public void mostrarInfo(){
+        System.out.println("Equipo favorito");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Color del uniforme: " + color);
+        System.out.println("Años de Fundacion: " + años);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        EquipoFavorito equipoFavorito = new EquipoFavorito("Club Deportivo El Nacional",
+                "Rojo", 60);
+        equipoFavorito.mostrarInfo();
+    }
+}
